839e61f95be18a9b608bc8c3fb37e9d289866b56
salmanxfxa <salmanxfxa@gmail.com>
2021-09-27 22:24:50 +0700

    Upload file CI

---
+++ application/controllers/Contoh.php
@@ -0,0 +1,14 @@
+<?php 
+
+Class Contoh extends CI_Controller{
+
+	public function index()
+	{
+		echo "ini method index";
+	}
+
+	public function mahasiswa()
+	{
+		echo "ini method mahasiswa";
+	}
+}
